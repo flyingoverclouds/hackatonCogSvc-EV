a31f8933dcd6ecc72698507d99d95a4e8fe48088
Nicolas CLERC <nicolas.clerc@sablefin.net>
2020-06-05 18:11:23 +0200

    initial program

---
+++ DemoCogSvcVision/DemoSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoCogSvcVision
+{
+    partial class DemoSettings
+    {
+        // This class is empty
+        // Create a DemoSettings.local.cs and add the following const : 
+        //      public const string csVisionKey = "YOURKEY";
+        //      public const string csVisionEndpoint = "https://YOURENDPOINT.cognitiveservices.azure.com/";
+        //      public const string csVisionLocation = "YOURLOCATION";
+    }
+}
---
+++ DemoCogSvcVision/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoCogSvcVision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** Demo Cognitive Service Vision");
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("ERROR argument missing : add image path as parameters");
+                return;
+            }
+            Console.WriteLine($"Vision ENDPOINT : {DemoSettings.csVisionEndpoint}");
+            Console.WriteLine($"Vision LOCATION : {DemoSettings.csVisionLocation}");
+            Console.WriteLine($"Vision KEY      : {DemoSettings.csVisionKey}");
+
+            Console.Write("Analyzing " +  args[0] + " ...");
+
+#if DEBUG
+            //Console.WriteLine("\n==> Press enter to exit");
+            //Console.ReadLine();
+#endif
+        }
+    }
+}
